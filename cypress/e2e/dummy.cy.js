60351c0342a080c737a0fdc49bd9a2731c14a430
Mohammed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2024-11-11 20:31:34 +0200

    Setup the project

--- cypress/e2e/dummy.cy.js
+++ cypress/e2e/dummy.cy.js
@@ -1,12 +1,16 @@
 describe('dummy test', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
     it('Correct title', () => {
-        cy.visit("http://localhost:5173")
         cy.title().should('eq', 'TeleGrammy')
     })
 
     it('Incorrect title', () => {
-        cy.visit("http://localhost:5173")
         cy.title().should('eq', 'Whatsuppy!')
+
     })
 
 })
